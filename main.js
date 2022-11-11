9a8c1c1da53464fdcba0dc422d1596ebc631971f
Gideana <celina.mayrhuber@gmail.com>
2022-11-11 17:29:44 +0000

    Add new grid options

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const container = document.querySelector('#container');
+const btn = document.querySelector('button');
 
-for (i=0; i<(16*16); i++) {
-    const square = document.createElement('div');
-    square.className = 'square';
-    square.textContent = '';
-    container.appendChild(square);
-    square.addEventListener('mouseover', (event) =>{
-        event.target.style.backgroundColor = 'black';
+
+window.onload = createGrid(16);
+
+function createGrid(z) {
+    for (i=0; i<(z*z); i++) {
+        const square = document.createElement('div');
+        square.id = 'square';
+        container.appendChild(square);
+        square.addEventListener('mouseover', (event) =>{
+            event.target.style.backgroundColor = 'black';
+        });
+    };
+    document.getElementById("container").style.gridTemplateColumns = `repeat(${z}, 1fr)`;
+    document.getElementById("container").style.gridTemplateRows = `repeat(${z}, 1fr)`;
+};
+
+btn.addEventListener('click', function() {
+    container.innerHTML='';
+    let z = prompt('Please select number of squares per side for the new grid (max. 100)');
+    if (z > 100) {
+        alert('Maximum number possible = 100, please select a new number');
+    } else {
+    return(createGrid(z));
     }
-    )
-}
-
+});
